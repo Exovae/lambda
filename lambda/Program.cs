a6326966a1a976ce7243464e791c59d1642b6f9b
Exovae <elliot@keymetricsoft.com>
2023-03-11 16:49:54 -0500

    Add project files.

---
+++ lambda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LambdaExpressionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Func<double, double, double> add = (x, y) => x + y;
+            Console.WriteLine($"The sum of the numbers is: {add(num1, num2)}");
+
+            Func<double, double, double> multiply = (x, y) => x * y;
+            Console.WriteLine($"The product of the numbers is: {multiply(num1, num2)}");
+
+            Func<double, double, double> findSmaller = (x, y) =>
+            {
+                if (x < y)
+                {
+                    return x;
+                }
+                else
+                {
+                    return y;
+                }
+            };
+            Console.WriteLine($"The smaller of the two numbers is: {findSmaller(num1, num2)}");
+        }
+    }
+}
